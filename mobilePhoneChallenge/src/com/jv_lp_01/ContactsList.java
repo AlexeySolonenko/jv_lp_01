caad83433db37f45ea9c523d53c84aceafee3751
Alexey Solonenko <ot2@bk.ru>
2019-02-11 20:34:18 +0100

    refactoring

---
+++ mobilePhoneChallenge/src/com/jv_lp_01/ContactsList.java
@@ -0,0 +1,51 @@
+package com.jv_lp_01;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ContactsList {
+
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+    public ContactsList() {
+
+    }
+
+    public void addContact(){
+
+    }
+    public int findMaxContactId(){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < contacts.size(); i++){
+
+        }
+    }
+    public void modifyContact(String name){
+
+    }
+
+    public int pickContact(){
+
+    }
+
+    public void printContacts(){
+
+    }
+
+    public void removeContact(){
+
+    }
+
+    public void selectContact(){
+
+    }
+
+
+//    public ArrayList<Contact> getContacts() {
+//        return contacts;
+//    }
+//
+//    public void setContacts(ArrayList<Contact> contacts) {
+//        this.contacts = contacts;
+//    }
+}
---
+++ mobilePhoneChallenge/src/com/jv_lp_01/MobilePhone.java
@@ -0,0 +1,13 @@
+package com.jv_lp_01;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+    public MobilePhone(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+
+}
